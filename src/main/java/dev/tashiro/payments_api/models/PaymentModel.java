0ed5303375ff889021b4cf8032722f59157cc03a
tashiro <andre_seichi@hotmail.com>
2024-02-17 15:24:08 -0300

    refactor: model payment

---
+++ src/main/java/dev/tashiro/payments_api/models/PaymentModel.java
@@ -0,0 +1,45 @@
+package dev.tashiro.payments_api.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+@Entity(name = "payments")
+public class PaymentModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private int debitCode;
+
+  @Length(min = 11, max = 14)
+  private String payerCpfCnpj;
+
+  @Length(max = 14)
+  private String paymentMethod;
+
+  @CreditCardNumber
+  private String cardNumber;
+
+  @Positive
+  @Min(value = 0)
+  private long paymentValue;
+
+  private String status = "Pendente de Processamento";
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}

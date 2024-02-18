57cf879f01370b6363730dbc3179238ec18749d3
tashiro <andre_seichi@hotmail.com>
2024-02-18 15:01:11 -0300

    feat: rota criação pagamento

--- src/main/java/dev/tashiro/payments_api/controllers/PaymentController.java
+++ src/main/java/dev/tashiro/payments_api/controllers/PaymentController.java
@@ -1,20 +1,33 @@
 package dev.tashiro.payments_api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.tashiro.payments_api.entities.PaymentEntity;
+import dev.tashiro.payments_api.dto.PaymentDTO;
+import dev.tashiro.payments_api.services.PaymentService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/payments")
 public class PaymentController {
 
+  @Autowired
+  private PaymentService paymentService;
+
   @PostMapping()
-  public void create(@Valid @RequestBody PaymentEntity paymentEntity) {
-    System.out.println(paymentEntity.getPaymentMethod());
+  public ResponseEntity<Object> create(@Valid @RequestBody PaymentDTO paymentDTO) {
+    try {
+      paymentService.create(paymentDTO);
+
+      return new ResponseEntity<>("OK", HttpStatus.CREATED);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
   }
 
 }

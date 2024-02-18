79f6f8309b09700214c7978b9084dffb51b507e1
tashiro <andre_seichi@hotmail.com>
2024-02-18 15:18:26 -0300

    feat: enum metodo de pagamento

--- src/main/java/dev/tashiro/payments_api/dto/PaymentDTO.java
+++ src/main/java/dev/tashiro/payments_api/dto/PaymentDTO.java
@@ -6,11 +6,12 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record PaymentDTO(@NotNull(message = "Deve conter código de débito") int debitCode,
         @NotBlank(message = "Deve conter CPF/CNPJ do pagador") @Size(min = 11, max = 14, message = "CPF/CNPJ deve ser entre 11 e 14 caracteres") String payerCpfCnpj,
-        @NotBlank(message = "Deve conter método de pagamento") @Size(max = 14, message = "Método de pagamento deve ter no máximo 14 caracteres") String paymentMethod,
+        @NotBlank(message = "Deve conter método de pagamento") @Size(max = 14, message = "Método de pagamento deve ter no máximo 14 caracteres") @Pattern(regexp = "^(boleto|pix|cartao_credito|cartao_debito)$", message = "Método de pagamento inválido") String paymentMethod,
         Optional<@CreditCardNumber(message = "Número do cartão de crédito inválido") String> cardNumber,
         @NotNull(message = "Deve conter valor do pagamento") long paymentValue) {
 

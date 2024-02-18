3e53c1539b5bdbb3f451c41fdcc587622e0e1a63
tashiro <andre_seichi@hotmail.com>
2024-02-18 15:00:12 -0300

    fix: modelagem Payment e constructor

--- src/main/java/dev/tashiro/payments_api/models/Payment.java
+++ src/main/java/dev/tashiro/payments_api/models/Payment.java
@@ -7,22 +7,38 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.CreditCardNumber;
 import org.hibernate.validator.constraints.Length;
 
+import dev.tashiro.payments_api.dto.PaymentDTO;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "payments")
-public class PaymentModel {
+public class Payment {
+
+  public Payment(PaymentDTO paymentDTO) {
+    this.debitCode = paymentDTO.debitCode();
+    this.payerCpfCnpj = paymentDTO.payerCpfCnpj();
+    this.paymentMethod = paymentDTO.paymentMethod();
+    this.cardNumber = paymentDTO.cardNumber().orElse(null);
+    this.paymentValue = paymentDTO.paymentValue();
+    this.createdAt = LocalDateTime.now();
+  }
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
+  @Column(unique = true)
   private int debitCode;
 
   @Length(min = 11, max = 14)

1cb1aa4fe3a6ee7011a008f910e957f01d61ee70
tashiro <andre_seichi@hotmail.com>
2024-02-18 15:09:59 -0300

    chore: format

--- src/main/java/dev/tashiro/payments_api/dto/PaymentDTO.java
+++ src/main/java/dev/tashiro/payments_api/dto/PaymentDTO.java
@@ -9,9 +9,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record PaymentDTO(@NotNull(message = "Deve conter código de débito") int debitCode,
-    @NotBlank(message = "Deve conter CPF/CNPJ do pagador") @Size(min = 11, max = 14, message = "CPF/CNPJ deve ser entre 11 e 14 caracteres") String payerCpfCnpj,
-    @NotBlank(message = "Deve conter método de pagamento") @Size(max = 14, message = "Método de pagamento deve ter no máximo 14 caracteres") String paymentMethod,
-    Optional<@CreditCardNumber(message = "Número do cartão de crédito inválido") String> cardNumber,
-    @NotNull(message = "Deve conter valor do pagamento") long paymentValue) {
+        @NotBlank(message = "Deve conter CPF/CNPJ do pagador") @Size(min = 11, max = 14, message = "CPF/CNPJ deve ser entre 11 e 14 caracteres") String payerCpfCnpj,
+        @NotBlank(message = "Deve conter método de pagamento") @Size(max = 14, message = "Método de pagamento deve ter no máximo 14 caracteres") String paymentMethod,
+        Optional<@CreditCardNumber(message = "Número do cartão de crédito inválido") String> cardNumber,
+        @NotNull(message = "Deve conter valor do pagamento") long paymentValue) {
 
 }
